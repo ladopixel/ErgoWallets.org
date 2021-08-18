800a0848afc2be33661174c9a8e31c4ae5537a46
Eneakao <info@untitled.es>
2021-08-18 13:51:43 +0200

    creando nuevamente el objeto del token

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,36 @@
 <script>
 
-	let infoWallet = {id: '', address: ''}
+	import Footer from './footer.svelte'
+
+	let ObjetoInfoErgo = {}
+	let infoWallet = {id: '', name: '', address: ''}
 	let arrayWallets = []
 	let valorWallet = ''
+	let nameWallet = ''
 	let objetoAddress = {}
 	let objetoToken = {}
 	let arrayTokens = []
+
+	const loadCoin = async() => {
+		const res = await fetch(`https://api.ergoplatform.com/api/v0/info`)
+		const data = await res.json()
+		if (res.ok){
+			ObjetoInfoErgo = {
+				hashRateErgo: data.hashRate,
+				transactionsErgo: data.transactionAverage,
+				totalCoinsErgo: new Intl.NumberFormat().format((data.supply/1000000000)),
+			}
+		}
+		const res2 = await fetch(`https://api.nanopool.org/v1/ergo/prices`)
+		const data2 = await res2.json()
+		if (res2.ok){
+			ObjetoInfoErgo.EurErgo = data2.data.price_eur
+			ObjetoInfoErgo.UsdErgo = data2.data.price_usd
+		}
+
+	}
+
+	loadCoin()
 
 	if (localStorage.getItem("arrayWallets")){
 		arrayWallets = JSON.parse(localStorage.getItem("arrayWallets"))
@@ -15,73 +40,54 @@
 	// Add wallets
 	const addWallet = async() => {
 		// Si la consulta devuelve ok 200 es verdadera
-		const res = await fetch(`https://api.ergoplatform.com/api/v1/addresses/${valorWallet}/balance/total`)
+		const res = await fetch(`https://api.ergoplatform.com/api/v1/addresses/${valorWallet}/balance/confirmed`)
 		if (res.ok){
 			const data = await res.json()
 			infoWallet.id = Date.now()
 			infoWallet.address = valorWallet
+			infoWallet.name = nameWallet
 			arrayWallets = [...arrayWallets, infoWallet]
-			infoWallet = {id: '', address: ''}
-			alert('Wallet add correct!')
+			infoWallet = {id: '', name: '', address: ''}
 		}else {
 			alert('Wallet incorrect!')
 		}
 	}
 
-
 	// Load wallet
 	const loadWallet = async() => {
 		// Si la consulta devuelve ok 200 es verdadera
-		const res = await fetch(`https://api.ergoplatform.com/api/v1/addresses/${valorWallet}/balance/total`)
+		//const res = await fetch(`https://api.ergoplatform.com/api/v1/addresses/${valorWallet}/balance/total`)
+		const res = await fetch(`https://api.ergoplatform.com/api/v1/addresses/${valorWallet}/balance/confirmed`)
+		const data = await res.json()
 		if (res.ok){
-			const data = await res.json()
 			// Objeto datos confirmados
 			objetoAddress = {
-				nanoErgsConf: data.confirmed.nanoErgs,
-				numberTokensConf: data.confirmed.tokens.length,
-				nanoErgsUnconf: data.unconfirmed.nanoErgs,
-				numberTokensUnconf: data.unconfirmed.tokens.length
+				nanoErgsConf: data.nanoErgs,
+				numberTokensConf: data.tokens.length,
 			}
 
 			// Si hay tokens confirmados
-			if (data.confirmed.tokens.length){
+			if (data.tokens.length){
 			 	arrayTokens = []
-				for (let i = 0; i < data.confirmed.tokens.length; i++) {
-					const res2 = await fetch(`https://api.ergoplatform.com/api/v1/boxes/byTokenId/${data.confirmed.tokens[i].tokenId}`)
+				for (let i = 0; i < data.tokens.length; i++) {
+					//const res2 = await fetch(`https://api.ergoplatform.com/api/v1/tokens/${data.confirmed.tokens[i].tokenId}`)
+					//const res2 = await fetch(`https://api.ergoplatform.com/api/v1/boxes/byTokenId/${data.confirmed.tokens[i].tokenId}`)
+					const res2 = await fetch(`https://api.ergoplatform.com/api/v0/assets/${data.tokens[i].tokenId}/issuingBox`)
+					const data2 = await res2.json()
 					if (res2.ok){
-						const data2 = await res2.json()
 						objetoToken = {
-							tokenCreationHeightConf: data2.items[0].creationHeight,
-							tokenTokenIdConf: data2.items[0].assets[0].tokenId,
-							tokenNameConf: data2.items[0].assets[0].name,
-							tokenAmountConf: data2.items[0].assets[0].amount,
-							tokenDecimalsConf: data2.items[0].assets[0].decimals,
-							tokenTypeConf: data2.items[0].assets[0].type,
+							tokenCreationHeightConf: data2[0].creationHeight,
+							tokenTokenIdConf: data2[0].assets[0].tokenId,
+							tokenAmountConf: data2[0].assets[0].amount,
+							tokenNameConf: data2[0].assets[0].name,
+							tokenDecimalsConf: data2[0].assets[0].decimals,
+							tokenTypeConf: data2[0].assets[0].type,
+							tokenR5Conf: toUtf8String(data2[0].additionalRegisters.R5).substr(2),
+							tokenR6Conf: data2[0].additionalRegisters.R6,
+							tokenR8Conf: data2[0].additionalRegisters.R8,
+							tokenR9Conf: toUtf8String(data2[0].additionalRegisters.R9).substr(2),
+							tokenTransactionConf: 'https://explorer.ergoplatform.com/en/transactions/' + data2[0].spentTransactionId
 						}
-						if (data2.items[0].additionalRegisters.R4){
-							objetoToken.tokenR4SigmaConf = data2.items[0].additionalRegisters.R4.sigmaType
-							objetoToken.tokenR4Conf = data2.items[0].additionalRegisters.R4.renderedValue
-						}
-						if (data2.items[0].additionalRegisters.R5){
-							objetoToken.tokenR5SigmaConf = data2.items[0].additionalRegisters.R5.sigmaType
-							objetoToken.tokenR5Conf = data2.items[0].additionalRegisters.R5.renderedValue
-						}
-						if (data2.items[0].additionalRegisters.R6){
-							objetoToken.tokenR6SigmaConf = data2.items[0].additionalRegisters.R6.sigmaType
-							objetoToken.tokenR6Conf = data2.items[0].additionalRegisters.R6.renderedValue
-						}
-						if (data2.items[0].additionalRegisters.R7){
-							objetoToken.tokenR7SigmaConf = data2.items[0].additionalRegisters.R7.sigmaType
-							objetoToken.tokenR7Conf = data2.items[0].additionalRegisters.R7.renderedValue
-						}
-						if (data2.items[0].additionalRegisters.R8){
-							objetoToken.tokenR8SigmaConf = data2.items[0].additionalRegisters.R8.sigmaType
-							objetoToken.tokenR8Conf = data2.items[0].additionalRegisters.R8.renderedValue
-						}
-						if (data2.items[0].additionalRegisters.R9){
-							objetoToken.tokenR9SigmaConf = data2.items[0].additionalRegisters.R9.sigmaType
-							objetoToken.tokenR9Conf = toUtf8String(data2.items[0].additionalRegisters.R9.renderedValue)
-						}	
 					}
 				arrayTokens[i] = objetoToken
 				}
@@ -125,70 +131,90 @@
 <svelte:head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
 </svelte:head>
 
 <main>
 	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
 		<div class="container-fluid">
-			<a href="https://ergonfts.org" class="mb-1 mx-3 separaI"><img src="ergo.png" alt="Logotype Ergo" width="100"></a>
+			<a href="https://ergowallets.org" class="mb-1 mx-3 separaI"><img src="ergo.png" alt="Logotype Ergo" width="100"></a>
 		  	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 		  	</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item dropdown">
+					<li class="nav-item dropdown ">
 						<a class="nav-link dropdown-toggle text-secondary" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 						<span class="text-secondary">Your Wallets</span>
 						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<ul class="dropdown-menu bg-light border border-secondary" aria-labelledby="navbarDropdown">
 							{#each arrayWallets as wallet}
-								<li><a on:click={muestroWallet(wallet.address)} class="dropdown-item" href="#">{wallet.address.substring(0, 7)}...{wallet.address.slice(-7)}</a></li>
+								<li><a on:click={muestroWallet(wallet.address)} class="dropdown-item text-dark small" href="#">{wallet.name}</a></li>
 							{/each}
-							<li><hr class="dropdown-divider"></li>
-							<li><a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#addWalletModal">Add wallet</a></li>
-							<li><a class="dropdown-item" on:click={cleanLocalStorage}>Delete wallet</a></li>
-							<li><hr class="dropdown-divider"></li>
-							<li><a class="dropdown-item" href="https://ergoplatform.org/en/wallets/" target="_blank">ErgoPlatform Wallets</a></li>
+							<li><hr class="dropdown-divider text-info"></li>
+							<li><a class="dropdown-item text-dark small" href="#" data-bs-toggle="modal" data-bs-target="#addWalletModal">Add wallet</a></li>
+							<li><a class="dropdown-item text-dark small" href="#" data-bs-toggle="modal" data-bs-target="#deleteWalletModal">Delete wallet</a></li>
+							<li><hr class="dropdown-divider text-danger"></li>
+							<li><a class="dropdown-item text-dark small" href="https://ergoplatform.org/en/wallets/" target="_blank">ErgoPlatform Wallets</a></li>
 						</ul>
 					</li>
 				</ul>
 				<input bind:value={valorWallet} class="form-control me-2" type="search" placeholder="Your wallet" aria-label="Your wallet">
-				<button on:click={loadWallet} class="btn btn-outline-success" type="submit">Accept</button>
+				<button on:click={loadWallet} class="btn btn-outline-info" type="submit"> Accept</button>
 			</div>
 		</div>
 	</nav>
 
-	<!-- Info wallet  -->
-	<ul class="list-group mt-3 bg-light " id="grupoInfo">
-    	<li class="list-group-item h4 text-secondary">
-        	  <span>Information</span>
+	<!-- Info Ergo -->
+	<ul class="list-group mt-3 bg-dark" id="grupoInfo">
+    	<li class="list-group-item bg-dark border-light">
+			<span class="small text-secondary"><strong>Price: </strong><span class="small text-light">{ObjetoInfoErgo.EurErgo}€ <i class="bi bi-arrow-right-short"></i> ${ObjetoInfoErgo.UsdErgo} </span>  <strong>Circulating supply: </strong><span class="small text-light">{ObjetoInfoErgo.totalCoinsErgo}</span> <strong>Transactions per Day: </strong><span class="small text-light">{ObjetoInfoErgo.transactionsErgo}</span>  <strong>Hashrate: </strong><span class="small text-light">{(ObjetoInfoErgo.hashRateErgo/1000000000000).toFixed(2)} TH/s</span></span>
         </li>
-		<li class="list-group-item ">
-			<span><strong>Address: </strong></span>
-			{#if (valorWallet)}
-				<span id="ergos">{valorWallet}</span>
-			{:else}
-				<span id="ergos"> </span>
-			{/if}
-		</li>
-		<li class="list-group-item ">
-			<span><strong>Ergos: </strong></span>
+	</ul>
+	<!-- Fin info Ergo-->
+
+	<!-- Info wallet  -->
+	<ul class="list-group mt-3 bg-light border border-light" id="grupoInfo">
+		<li class="list-group-item bg-dark">
 			{#if (objetoAddress.nanoErgsConf)}
-				<span id="ergos">{objetoAddress.nanoErgsConf/1000000000}</span>
+				<span class="small text-info ">{(objetoAddress.nanoErgsConf/1000000000).toFixed(2)}ERG <i class="bi bi-arrow-right-short"></i> {((objetoAddress.nanoErgsConf/1000000000)*ObjetoInfoErgo.EurErgo).toFixed(2)}€ <i class="bi bi-arrow-right-short"></i> ${((objetoAddress.nanoErgsConf/1000000000)*ObjetoInfoErgo.UsdErgo).toFixed(2)}</span>
 			{:else}
-				<span id="ergos">0</span>
+				<span class="small text-info">0.00</span>
 			{/if}
 		</li>
-		<li class="list-group-item ">
-			<span><strong>Tokens: </strong></span>
+		<li class="list-group-item bg-secondary text-secondary">
+			<span class="text-light small"><strong>Tokens </strong></span>
 			{#if (objetoAddress.numberTokensConf)}
 				<span id="tokens">{objetoAddress.numberTokensConf}</span>
-				<ul class="list-group">
+				<ul class="list-group mt-2 small">
 					{#await loadWallet}
 						<span>Loading...</span>
 					{:then loadWallet}
 						{#each arrayTokens as datos}
-							<li class="list-group-item">{datos.tokenTokenIdConf}</li>
+							<li class="list-group-item bg-light">
+							{#if (datos.tokenR9Conf)}
+								<img class="img-thumbnail my-2" width=100 src={datos.tokenR9Conf} alt={datos.name}>
+								<br>
+							{/if}
+							<span><strong>Creation Height: </strong>{datos.tokenCreationHeightConf}</span>
+							<br>
+							<span class="text-break"><strong>Id:</strong> {datos.tokenTokenIdConf}</span>
+							<br>
+							{#if (datos.tokenR8Conf)}
+								<span class="text-break"><strong>Sha256: </strong>{datos.tokenR8Conf}</span>
+								<br>
+							{/if}
+							<span><strong>Name: </strong>{datos.tokenNameConf}</span>
+							<br>
+							<span><strong>Amount: </strong>{datos.tokenAmountConf}</span> 
+							<span><strong>Decimals: </strong>{datos.tokenDecimalsConf}</span>
+							<span><strong>Type: </strong>{datos.tokenTypeConf}</span>
+							<br>
+							<span class="text-break"><strong>Description: </strong>{datos.tokenR5Conf}</span>
+							<br>
+							<span class="text-break"><strong>Transaction: </strong><a href={datos.tokenTransactionConf} target="_blank">{datos.tokenTransactionConf.substr(50)}</a></span>
+							
+							</li>
 						{/each}
 					{:catch error}
 						<p class="text-secondary">Something went wrong: {error.message}</p>
@@ -203,7 +229,7 @@
 
 
 	  <!-- Transaciones -->
-	  <ul class="list-group mt-3 bg-secondary " id="grupoTransactions">
+	  <!-- <ul class="list-group mt-3 bg-secondary border border-light " id="grupoTransactions">
         <li class="list-group-item h4 bg-secondary text-light">
           <span>Transactions</span>
         </li>
@@ -223,24 +249,56 @@
 					<span id="tokens">0</span>
 				{/if}
 			</li>
-      </ul>
+      </ul> -->
 	  <!-- Fin Transaciones -->
 </main>
+
+<Footer />
 
 <!-- Modal -->
 <div class="modal fade" id="addWalletModal" tabindex="-1" aria-labelledby="addWalletModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 	  <div class="modal-content">
-		<div class="modal-header">
-		  <h5 class="modal-title" id="addWalletModalLabel">Add wallet</h5>
-		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-		</div>
+		<div class="modal-header bg-secondary">
+			<h5 class="modal-title bg-dark text-light py-1 px-3" id="exampleModalLabel">Add wallets</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		  </div>
 		<div class="modal-body">
-			<input bind:value={valorWallet} placeholder="Your wallet">
+			<div class="container">
+				<div class="row">
+				  <div class="row pb-2">
+					<span class="mt-2">Name: </span><input bind:value={nameWallet} placeholder="Your address name">
+					<span class="mt-2">Address: </span><input bind:value={valorWallet} placeholder="Your address">
+				  </div>
+				</div>
+			  </div>
 		</div>
 		<div class="modal-footer">
-		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-		  <button on:click={addWallet} type="button" class="btn btn-primary">Accept</button>
+		  <button type="close" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+		  <button on:click={addWallet} type="close" data-bs-dismiss="modal" class="btn btn-info">Accept</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+  <!-- Modal Delete wallets-->
+<div class="modal fade" id="deleteWalletModal" tabindex="-1" aria-labelledby="deleteWalletModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header bg-secondary">
+			<h5 class="modal-title bg-dark text-light py-1 px-3" id="exampleModalLabel">Delete wallets</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		  </div>
+		<div class="modal-body">
+			<div class="container">
+				<div class="row">
+				  <span>Are you sure you want to delete your addresses?</span>
+				</div>
+			  </div>
+		</div>
+		<div class="modal-footer">
+		  <button type="close" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+		  <button on:click={cleanLocalStorage} type="close" data-bs-dismiss="modal" class="btn btn-info">Delete</button>
 		</div>
 	  </div>
 	</div>
---
+++ src/footer.svelte
@@ -0,0 +1,96 @@
+<script>
+	let imageDonation = 'QR-donate-9ggm3WB6e2BMbbCD3C9PJcgm5wbvmvCUG7w5rgszbKVWrVr8fSE.png'
+</script>
+<center>
+	<small>
+		<button type="button" title="FAQ" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modalFaq">FAQ</button>
+		<a href="https://ergoplatform.org" title="ergoplatform.org" class="text-secondary">
+			<img src="Ergo-miniatura.png" title="ergoplatform.org" width="25" class="my-2 mx-2" alt="Logotype Ergo">ergoplatform.org
+		</a>
+		<a href="https://github.com/ladopixel/ergotokens" title="ergotokens.org Github" class="text-secondary">
+			<img src="Github.png" title="ergotokens.org Github " width="25" class="my-2 mx-2" alt="Logotype Github">Github
+		</a>
+		<button type="button" title="Donate :)" data-bs-toggle="modal" class="btn btn-dark" data-bs-target="#modalDonate"> <i class="bi bi-hand-thumbs-up-fill"></i></button>
+		<a href="https://discord.gg/Na9DPgRvA9" target="_blank" title="Discord ErgoPlatform" class="btn btn-dark text-light"><i class="bi bi-discord"></i></a>
+        <a href="https://twitter.com/ergoplatformorg" target="_blank" title="Twitter ErgoPlatform" class="btn btn-dark text-light"><i class="bi bi-twitter"></i></a>
+
+	</small>
+</center>
+
+  
+ <!-- Modal -->
+  <div class="modal fade " id="modalDonate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+	  <div class="modal-content">
+		<div class="modal-header bg-secondary">
+		  <h5 class="modal-title bg-dark text-light py-1 px-3" id="exampleModalLabel">Donate ERG</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+			<div class="text-break pb-3">If you liked the website <strong>you can invite me to a coffee,</strong> I will be happy to drink it to your health.</div>
+			<div><span class="small"><strong>ERG address: </strong>9ggm3WB6e2BMbbCD3C9PJcgm5wbvmvCUG7w5rgszbKVWrVr8fSE</span></div>
+			<div><img src={imageDonation} alt="QR wallet address"/></div>
+
+		</div>
+	  </div>
+	</div>
+  </div>
+
+  <!-- Modal -->
+	<div class="modal fade " id="modalFaq" tabindex="-1" aria-labelledby="exampleModalLabelFaq" aria-hidden="true">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content bg-light">
+				<div class="modal-header bg-secondary">
+					<h5 class="modal-title bg-dark text-light py-1 px-3" id="exampleModalLabelFaq">FAQ</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div class="accordion" id="accordionFlushFaq">
+						<p><small>ErgoTokens.org is a website created by the community to <strong>view information on tokens</strong> created on the Ergo blockchain, for other questions or improvements you can <a href="https://github.com/ladopixel/ergotokens/issues" target="_blank">create an issues on github.</a></small></p>
+						
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="flush-headingOne">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+									What can I do in ergotokens.org?
+								</button>
+							</h2>
+							<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushFaq">
+								<div class="accordion-body">
+									<p>Ergotokens.org is used to view information about the tokens created on the Ergo blockchain, from here you can see <a href="https://github.com/ergoplatform/eips/blob/master/eip-0004.md" target="_blank">more details Assets standard.</a></p>
+									<ul>
+										<li>Details of a specific token >> <a href="https://ergotokens.org/#/?token=tokenID" target="_blank">https://ergotokens.org/#/?token=tokenID</a></li>
+									</ul>
+							</div>
+						</div>
+						
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="flush-headingTwo">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+									Can I sell or auction an NFT from this website?
+								</button>
+							</h2>
+							<div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushFaq">
+								<div class="accordion-body">
+									This is not possible from this website, you can auction your NFT at <a href="https://ergoauctions.org" target="_blank">https://ergoauctions.org.</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="flush-headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+									Can I create an token from this website?
+								</button>
+							</h2>
+							<div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushFaq">
+								<div class="accordion-body">
+									This is not possible from this website, you can create an NFT at <a href="https://ergoutils.org" target="_blank">https://ergoutils.org.</a>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
